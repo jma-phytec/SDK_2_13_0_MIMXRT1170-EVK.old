24e520baca301871bea33f7a664b1f2640005678
John Ma <110064859+jma-phytec@users.noreply.github.com>
2023-02-03 11:27:44 -0800

    ETH0 DHCP

--- boards/evkmimxrt1170/lwip_examples/lwip_dhcp/bm/cm7/board.h
+++ boards/evkmimxrt1170/lwip_examples/lwip_dhcp/bm/cm7/board.h
@@ -124,10 +124,10 @@ extern button_config_t g_buttonConfig[];
 #endif
 
 /*! @brief The ENET0 PHY address. */
-#define BOARD_ENET0_PHY_ADDRESS (0x02U) /* Phy address of enet port 0. */
+#define BOARD_ENET0_PHY_ADDRESS (0x01U) /* Phy address of enet port 0. */
 
 /*! @brief The ENET1 PHY address. */
-#define BOARD_ENET1_PHY_ADDRESS (0x01U) /* Phy address of enet port 1. */
+#define BOARD_ENET1_PHY_ADDRESS (0x00U) /* Phy address of enet port 1. */
 
 /*! @brief The EMVSIM SMARTCARD PHY configuration. */
 #define BOARD_SMARTCARD_MODULE                (EMVSIM1)      /*!< SMARTCARD communicational module instance */
@@ -203,7 +203,7 @@ extern button_config_t g_buttonConfig[];
 /* Below comment is for test script to easily define which port to be used, please don't delete. */
 /* @TEST_ANCHOR */
 #ifndef BOARD_NETWORK_USE_100M_ENET_PORT
-#define BOARD_NETWORK_USE_100M_ENET_PORT (0U)
+#define BOARD_NETWORK_USE_100M_ENET_PORT (1U)
 #endif
 
 /* Timer Manager definition. */
